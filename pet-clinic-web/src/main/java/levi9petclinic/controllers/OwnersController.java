1e9cba806f5e82dd9faf1dc7719fd1fad397b2e8
volodymyr-yastrubchak <volodymyry@levi9.com>
2022-10-21 12:31:25 +0300

    Display owners and vets

--- pet-clinic-web/src/main/java/levi9petclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/levi9petclinic/controllers/OwnersController.java
@@ -1,14 +1,24 @@
 package levi9petclinic.controllers;
 
+import levi9petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping({"/owners"})
 @Controller
 public class OwnersController {
 
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "index.html"})
-    public String ListOfOwners() {
+    public String ListOfOwners(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
--- pet-clinic-web/src/main/java/levi9petclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/levi9petclinic/controllers/VetsController.java
@@ -1,13 +1,24 @@
 package levi9petclinic.controllers;
 
+import levi9petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetsController {
 
+    private final VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listOfVets() {
+    public String listOfVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
