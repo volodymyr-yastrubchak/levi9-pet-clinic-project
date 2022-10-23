cb3b8f40fda7135245f979ba5a65ebaa9e99631e
volodymyr-yastrubchak <volodymyry@levi9.com>
2022-10-23 14:05:40 +0300

    refactor

--- pet-clinic-web/src/main/java/levi9petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/levi9petclinic/bootstrap/DataLoader.java
@@ -22,13 +22,11 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner = new Owner();
-        owner.setId(1L);
         owner.setFirstName("Ivan");
         owner.setLastName("Tarantino");
         ownerService.save(owner);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Stepan");
         owner2.setLastName("Buratino");
         ownerService.save(owner2);
@@ -37,13 +35,11 @@ public class DataLoader implements CommandLineRunner {
 
 
         Vet vet = new Vet();
-        vet.setId(1L);
         vet.setFirstName("Vesela");
         vet.setLastName("Veselka");
         vetService.save(vet);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Sum");
         vet2.setLastName("Sumnyi");
         vetService.save(vet2);
