6b4e671e7e330cd1e04509b8094d408090f82aa3
volodymyr-yastrubchak <volodymyry@levi9.com>
2022-10-25 15:27:18 +0300

    add @Entity to classes

--- pet-clinic-data/src/main/java/levi9petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/levi9petclinic/model/Vet.java
@@ -2,10 +2,22 @@ package levi9petclinic.model;
 
 import java.util.HashSet;
 import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
-    private Set<Speciality> specialities  = new HashSet<>();
+    @ManyToMany
+    @JoinTable(
+        name = "vets_apecialities",
+        joinColumns = @JoinColumn(name = "vet_id"),
+        inverseJoinColumns = @JoinColumn(name = "speciality_id"))
+    private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
         return specialities;
